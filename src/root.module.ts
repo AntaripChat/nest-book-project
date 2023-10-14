f73bd16327cb31235ab24691ef475090451d69f2
Antarip Chatterjee <chatterjeeantarip1234@gmail.com>
2023-10-14 21:11:26 +0530

    first commit

---
+++ src/root.module.ts
@@ -0,0 +1,17 @@
+import { Module } from "@nestjs/common";
+import { BookModule } from './book/book.module';
+
+
+
+@Module({
+    imports:[BookModule],
+    controllers:[],
+    providers:[],
+    exports:[]
+})
+
+export class RootModule{
+    constructor(){
+        console.log("Module");
+    }
+}
